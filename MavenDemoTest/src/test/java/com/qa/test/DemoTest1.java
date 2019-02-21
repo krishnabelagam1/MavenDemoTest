10502c692c256dd55eb28b2dde63fc9d126dd9e9
Dell <Dell@DESKTOP-HBF7D93>
2019-02-22 00:42:08 +0530

    First commit

---
+++ MavenDemoTest/src/test/java/com/qa/test/DemoTest1.java
@@ -0,0 +1,40 @@
+package com.qa.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest1 {
+	@Test
+	public void add() {
+		System.out.println("Additon");
+		int a = 10, b = 30;
+		Assert.assertEquals(40, a+b);
+	}
+
+	@Test
+	public void sub() {
+		System.out.println("Substraction");
+		int a = 10, b = 30;
+		Assert.assertEquals(20, b-a);
+	}
+	@Test
+	public void mul() {
+		System.out.println("Multiplication");
+		int a = 10, b = 30;
+		Assert.assertEquals(300, a*b);
+	}
+	@Test
+	public void div() {
+		System.out.println("Division");
+		int a = 10, b = 30;
+		Assert.assertEquals(3, b/a);
+	}
+	@Test
+	public void mod() {
+		System.out.println("Module");
+		int a = 10, b = 30;
+		Assert.assertEquals(0, b%a);
+	}
+
+
+}
